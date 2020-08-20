4f9ab9b1835f1c8147f3ed62408980791b9bf4db
gusma <gustavo.malamud@globant.com>
2020-08-20 01:13:36 -0300

    Adds ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,10 @@
-const mongodb = require("mongodb");
-
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID();
+console.log(id);
 
 MongoClient.connect(
 	connectionURL,
@@ -14,8 +15,11 @@ MongoClient.connect(
 		}
 		const db = client.db(databaseName);
 
+		// Adds an individual instance
+		//
 		// db.collection("users").insertOne(
 		// 	{
+		// 		_id: id,
 		// 		name: "Gustavo",
 		// 		age: 37,
 		// 	},
@@ -27,18 +31,19 @@ MongoClient.connect(
 		// 	}
 		// );
 
-		db.collection("users").insertMany(
-			[
-				{ description: "Do your dishes", completed: true },
-				{ description: "Take a bath", completed: false },
-				{ description: "Do your homework", completed: true },
-			],
-			(error, result) => {
-				if (error) {
-					return console.log("Failed to insert!");
-				}
-				console.log(result.ops);
-			}
-		);
+		// Adds multiple instances
+		// db.collection("users").insertMany(
+		// 	[
+		// 		{ description: "Do your dishes", completed: true },
+		// 		{ description: "Take a bath", completed: false },
+		// 		{ description: "Do your homework", completed: true },
+		// 	],
+		// 	(error, result) => {
+		// 		if (error) {
+		// 			return console.log("Failed to insert!");
+		// 		}
+		// 		console.log(result.ops);
+		// 	}
+		// );
 	}
 );
